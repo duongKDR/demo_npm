6f8b0822ccf57bf4d0fdeed5afa1c25a4acc04ee
duongKDR <106894490+duongKDR@users.noreply.github.com>
2022-10-21 11:38:41 +0700

    morgan

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 
 const express = require('express')
 const app = express()
-// require('dotenv').config()
-const port = process.env.PORT || 5000
 
+// require('dotenv').config()
+const port = process.env.PORT || 3000
+var morgan = require('morgan')
+ 
+
+app.use(morgan('combined'))
 // !important! 
 // you need to install the following libraries |express|[dotenv > if required]
 // or run this command >> npm i express dotenv 
-
 app.get('/' , (req , res)=>{
 
    res.send('hello from simple server :)')
 
 })
+
 
 
 app.listen(port , ()=> console.log('> Server is up and running on port : ' + port))
